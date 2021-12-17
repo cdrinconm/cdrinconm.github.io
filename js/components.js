89402f43339e2267b17c96f5553268342d82c6c9
Carlos Rincon <cdrinconm@unal.edu.co>
2021-12-16 23:44:43 -0500

    Incluir VC

--- js/components.js
+++ js/components.js
@@ -31,19 +31,21 @@ class CRHeader extends HTMLElement{
                         </button>
                         <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
                             <div class="offcanvas-header">
-                            <h5 class="offcanvas-title" id="offcanvasNavbarLabel">Menu</h5>
-                            <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                                <div class="jumbotron menu">
+                                    <h5 class="offcanvas-title" id="offcanvasNavbarLabel">Menu</h5></br>
+                                    <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                                </div>
                             </div>
                             <div class="offcanvas-body">
                             <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
                                 <li class="nav-item">
-                                <a class="nav-link active" aria-current="page" href="./index.html">Inicio</a>
+                                    <a class="nav-link active" aria-current="page" href="./index.html">Inicio</a>
                                 </li>
                                 <li class="nav-item">
-                                <a class="nav-link" href="./cv.html">CV</a>
+                                <a class="nav-link" href="./css.html">Diseños en CSS</a>
                                 </li>
                                 <li class="nav-item">
-                                <a class="nav-link" href="./css.html">CSS</a>
+                                <a class="nav-link" href="https://cdrinconm.github.io/vc/" target="_blank">Computacion Visual</a>
                                 </li>
                                 <!--
                                 <li class="nav-item dropdown">
@@ -59,11 +61,10 @@ class CRHeader extends HTMLElement{
                                         <li><a class="dropdown-item" href="#">Something else here</a></li>
                                     </ul>
                                 </li>
-                            </ul>
-                            <form class="d-flex">
-                                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                                <button class="btn btn-outline-success" type="submit">Search</button>
-                            </form>-->
+                            </ul>-->
+                            <li class="nav-item">
+                                <a class="nav-link" href="./cv.html">Mi Hoja de Vida</a>
+                            </li>
                             </div>
                         </div>
                         </div>
